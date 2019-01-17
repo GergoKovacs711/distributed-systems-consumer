223d983b4778d2a6ef916d92030c3726ccc52ddd
Gergő Kovács <kovacs.gergo.0711@gmail.com>
2019-01-17 14:10:49 +0100

    Minor changes

--- src/main/java/hu/iit/uni/miskolc/kovacsgergo/distributedsystemconsumer/Application.java
+++ src/main/java/hu/iit/uni/miskolc/kovacsgergo/distributedsystemconsumer/Application.java
@@ -3,7 +3,6 @@ package hu.iit.uni.miskolc.kovacsgergo.distributedsystemconsumer;
 import hu.iit.uni.miskolc.kovacsgergo.distributedsystemconsumer.model.generated.ExchangeRate;
 import hu.iit.uni.miskolc.kovacsgergo.distributedsystemconsumer.model.generated.ObjectFactory;
 import hu.iit.uni.miskolc.kovacsgergo.distributedsystemconsumer.util.Util;
-import hu.iit.uni.miskolc.kovacsgergo.distributedsystemconsumer.util.exception.ApiError;
 import org.apache.commons.cli.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,16 +14,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.StringReader;
-import java.util.LinkedHashMap;
+import java.util.Iterator;
 import java.util.List;
 
 @SpringBootApplication
 public class Application {
-    static private Options options;
     static private CommandLine cmd;
 
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
@@ -38,13 +34,8 @@ public class Application {
 
     public static void main(String args[]) {
 
-        //Setting up commang line arguments
-        options = new Options();
-        options.addOption("a", false, "request all exchange rate data");
-        options.addOption("t", false, "request test action from the service");
-        options.addOption("s", false, "request single rate data from the service");
-        options.addOption("rm", false, "request to remove single rate data from the service");
-        options.addOption("c", false, "request to create single rate data from the service");
+        //Setting up command line arguments
+        Options options = createOptions();
 
         // parsing arguments
         CommandLineParser parser = new DefaultParser();
@@ -65,7 +56,7 @@ public class Application {
     }
 
     @Bean
-    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+    public CommandLineRunner run(RestTemplate restTemplate) {
         return args -> {
             if (cmd.hasOption("a")) {
                 listRequest(restTemplate, GET_ALL_RATE);
@@ -75,14 +66,14 @@ public class Application {
 
             } else if (cmd.hasOption("s")) {
                 try {
-                    listRequest(restTemplate, GET_RATE + cmd.getArgs()[0]);
+                    singleRequest(restTemplate, GET_RATE, cmd.getArgs()[0]);
                 } catch (ArrayIndexOutOfBoundsException e) {
                     logger.error("Argument is null");
                 }
 
             } else if (cmd.hasOption("rm")) {
                 try {
-                    listRequest(restTemplate, DELETE_RATE + cmd.getArgs()[0]);
+                    singleRequest(restTemplate, DELETE_RATE, cmd.getArgs()[0]);
                 } catch (ArrayIndexOutOfBoundsException e) {
                     logger.error("Argument is null");
                 }
@@ -102,31 +93,38 @@ public class Application {
 
     private void listRequest(RestTemplate restTemplate, final String REQUEST_MAPPING) {
         try {
-            ResponseEntity<List<Object>> rateResponse =
+            ResponseEntity<List<ExchangeRate>> rateResponse =
                     restTemplate.exchange(SERVICE_URL + REQUEST_MAPPING,
-                            HttpMethod.GET, null, new ParameterizedTypeReference<List<Object>>() {});
+                            HttpMethod.GET, null, new ParameterizedTypeReference<List<ExchangeRate>>() {});
 
 
+            if(rateResponse.getBody() != null) {
+                List<ExchangeRate> rates = rateResponse.getBody();
+                Iterator iterator = rates.iterator();
 
-            if (rateResponse.getBody() != null) {
+                while (iterator.hasNext()) {
+                    logger.info(Util.toString((ExchangeRate) iterator.next()));
+                }
+            }
+
+/*            if (rateResponse.getBody() != null) {
                 try {
                     List<Object> rates = rateResponse.getBody();
                     ObjectFactory objectFactory = new ObjectFactory();
                     for (Object rate : rates) {
                         LinkedHashMap<String, Object> map = (LinkedHashMap<String, Object>) rate;
-                       if(map.keySet().contains("rate")){
-                           ExchangeRate exchangeRate = objectFactory.createExchangeRate();
-                           exchangeRate.setRate((double)(map.get("rate")));
-                           exchangeRate.setName(map.get("name").toString());
-                           exchangeRate.setCode(map.get("code").toString());
-                           logger.info(Util.toString(exchangeRate));
-                        }else{
-                           logger.error(map.get("statusCode").toString());
-                           logger.error(map.get("statusCodeValue").toString());
-                           logger.error(map.get("body").toString());
+                        if (map.keySet().contains("rate")) {
+                            ExchangeRate exchangeRate = objectFactory.createExchangeRate();
+                            exchangeRate.setRate((double) (map.get("rate")));
+                            exchangeRate.setName(map.get("name").toString());
+                            exchangeRate.setCode(map.get("code").toString());
+                            logger.info(Util.toString(exchangeRate));
+                        } else {
+                            logger.error(map.get("statusCode").toString());
+                            logger.error(map.get("statusCodeValue").toString());
+                            logger.error(map.get("body").toString());
                         }
                     }
-
                 } catch (Exception e){
                     e.printStackTrace();
                 }
@@ -134,9 +132,10 @@ public class Application {
 
             } else {
                 logger.info("Response is null");
-            }
+            }*/
 
         } catch (Exception e) {
+            e.printStackTrace();
             logger.error("Failed to connect to the server!");
         }
     }
@@ -178,5 +177,47 @@ public class Application {
         } catch (Exception e) {
             logger.error("Failed to connect to the server!");
         }
+    }
+
+    private static void singleRequest(RestTemplate restTemplate, final String REQUEST_MAPPING, String argument){
+        try {
+            ResponseEntity<List<ExchangeRate>> rateResponse =
+                    restTemplate.exchange(SERVICE_URL + REQUEST_MAPPING + argument,
+                            HttpMethod.GET, null, new ParameterizedTypeReference<List<ExchangeRate>>() {});
+
+            if(rateResponse.getBody() != null) {
+                List<ExchangeRate> rates = rateResponse.getBody();
+                String console_info = null;
+
+                if(REQUEST_MAPPING.compareTo(DELETE_RATE) == 0){
+                    console_info = "The following exchange rate has been deleted: ";
+                } else if (REQUEST_MAPPING.compareTo(GET_RATE) == 0){
+                    console_info = "The following exchange rate has been found: ";
+                }
+
+                if(rates.get(0).getCode() == null){
+                    logger.info("No matching exchange rate has been found!");
+
+                } else {
+                    logger.info(console_info);
+                    logger.info(Util.toString(rates.get(0)));
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("Failed to connect to the server!");
+        }
+
+    }
+
+    private static Options createOptions(){
+        Options options = new Options();
+        options.addOption("a", false, "request all exchange rate data");
+        options.addOption("t", false, "request test action from the service");
+        options.addOption("s", false, "request single rate data from the service");
+        options.addOption("rm", false, "request to remove single rate data from the service");
+        options.addOption("c", false, "request to create single rate data from the service");
+        return options;
     }
 }
